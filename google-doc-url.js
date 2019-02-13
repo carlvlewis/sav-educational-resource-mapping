32088442bb0da226242fc9e0182939affe3b0d52
Carl V. Lewis <carl@carlvlewis.net>
2019-02-13 10:21:52 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JEXLesZZiUkUjRdR9KRlM9dW4hSWBwRj1C8GRAwY6M0/edit?usp=sharing';
